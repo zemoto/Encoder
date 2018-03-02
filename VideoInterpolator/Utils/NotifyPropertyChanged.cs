0ce0d41137b2579c45fa82fc83020967c36775a1
Unknown <zemoto@gmail.com>
2018-03-01 19:00:09 -0500

    +Initial solution and project

---
+++ VideoInterpolator/Utils/NotifyPropertyChanged.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using VideoInterpolator.Annotations;
+
+namespace VideoInterpolator.Utils
+{
+   internal class NotifyPropertyChanged : INotifyPropertyChanged
+   {
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      protected bool SetProperty( ref object property, object value, [CallerMemberName] string propertyName = null )
+      {
+         if ( property.Equals( value ) )
+         {
+            return false;
+         }
+
+         property = value;
+         OnPropertyChanged( propertyName );
+         return true;
+      }
+
+      [NotifyPropertyChangedInvocator]
+      protected void OnPropertyChanged( [CallerMemberName] string propertyName = null )
+      {
+         PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( propertyName ) );
+      }
+   }
+}
