d958ed4fddc285a8392ab7570acb2de0e7b1ed6d
Unknown <zemoto@gmail.com>
2018-08-06 22:39:23 -0400

    Use markup extension instead of object data providers

---
+++ Interpolator/Utils/GetEnumValuesExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace Interpolator.Utils
+{
+   internal sealed class BoundEnumMember
+   {
+      public BoundEnumMember( object value )
+      {
+         Display = value.ToString();
+         Value = value;
+      }
+
+      public string Display { get; set; }
+      public object Value { get; set; }
+   }
+
+   internal sealed class GetEnumValuesExtension : MarkupExtension
+   {
+      private readonly Type _type;
+      public GetEnumValuesExtension( Type type )
+      {
+         _type = type;
+      }
+
+      public override object ProvideValue( IServiceProvider serviceProvider )
+      {
+         var target = serviceProvider.GetService( typeof(IProvideValueTarget) ) as IProvideValueTarget;
+         if ( target != null && target.TargetObject is ComboBox )
+         {
+            ( (ComboBox)target.TargetObject ).SelectedValuePath = "Value";
+            ( (ComboBox)target.TargetObject ).DisplayMemberPath = "Display";
+         }
+
+         var enumValues = Enum.GetValues( _type );
+
+         return ( from object enumValue in enumValues
+                  select new BoundEnumMember( enumValue ) ).ToArray();
+      }
+   }
+}
