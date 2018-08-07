e14212c298b62cda18a061f8fefce8929cab276e
Unknown <zemoto@gmail.com>
2018-08-06 21:19:43 -0400

    Add properties for interpolate args

--- Interpolator/Filters/Interpolate/InterpolateEnums.cs
+++ Interpolator/Filters/Interpolate/InterpolateEnums.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Interpolator.Filters.Interpolate
+﻿namespace Interpolator.Filters.Interpolate
 {
    internal enum InterpolationMode
    {
--- Interpolator/Filters/Interpolate/InterpolateFilterViewModel.cs
+++ Interpolator/Filters/Interpolate/InterpolateFilterViewModel.cs
@@ -8,5 +8,40 @@
          get => _targetFrameRate;
          set => SetProperty( ref _targetFrameRate, value );
       }
+
+      private InterpolationMode _interpolationMode = InterpolationMode.MotionCompensated;
+      public InterpolationMode InterpolationMode
+      {
+         get => _interpolationMode;
+         set => SetProperty( ref _interpolationMode, value );
+      }
+
+      private MotionCompensationMode _motionCompensationMode = MotionCompensationMode.Adaptive;
+      public MotionCompensationMode MotionCompensationMode
+      {
+         get => _motionCompensationMode;
+         set => SetProperty( ref _motionCompensationMode, value );
+      }
+
+      private MotionEstimationMode _motionEstimationMode = MotionEstimationMode.Bidirectional;
+      public MotionEstimationMode MotionEstimationMode
+      {
+         get => _motionEstimationMode;
+         set => SetProperty( ref _motionEstimationMode, value );
+      }
+
+      private MotionEstimationAlgorithm _motionEstimationAlgorithm = MotionEstimationAlgorithm.Predictive;
+      public MotionEstimationAlgorithm MotionEstimationAlgorithm
+      {
+         get => _motionEstimationAlgorithm;
+         set => SetProperty( ref _motionEstimationAlgorithm, value );
+      }
+
+      private SceneChangeDetectionAlgorithm _sceneChangeDetectionAlgorithm = SceneChangeDetectionAlgorithm.FrameDifference;
+      public SceneChangeDetectionAlgorithm SceneChangeDetectionAlgorithm
+      {
+         get => _sceneChangeDetectionAlgorithm;
+         set => SetProperty( ref _sceneChangeDetectionAlgorithm, value );
+      }  
    }
 }
