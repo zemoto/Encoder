3348f38cc232079a95750351a70975189f70fb92
Unknown <zemoto@gmail.com>
2018-08-06 21:14:12 -0400

    Add enums for all interpolation args

---
+++ Interpolator/Filters/Interpolate/InterpolateEnums.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpolator.Filters.Interpolate
+{
+   internal enum InterpolationMode
+   {
+      Duplicate,
+      Blend,
+      MotionCompensated
+   }
+
+   internal enum MotionCompensationMode
+   {
+      OverlappedBlock,
+      Adaptive
+   }
+
+   internal enum MotionEstimationMode
+   {
+      Bidirectional,
+      Bilateral
+   }
+
+   internal enum MotionEstimationAlgorithm
+   {
+      Exhaustive,
+      ThreeStep,
+      TwoDimensional,
+      NewThreeStep,
+      FourStep,
+      Diamond,
+      Hexagon,
+      Predictive,
+      UnevenMultiHexagon
+   }
+
+   internal enum SceneChangeDetectionAlgorithm
+   {
+      None,
+      FrameDifference
+   }
+}
