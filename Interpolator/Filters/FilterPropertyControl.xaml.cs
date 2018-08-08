c00fd14e077f3dfeae868f9a890f1d3c50a649c4
David Markachev <zemoto@gmail.com>
2018-08-08 16:42:39 -0400

    Add custom control for filter properties

---
+++ Interpolator/Filters/FilterPropertyControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Interpolator.Filters
+{
+   internal partial class FilterPropertyControl
+   {
+      public static readonly DependencyProperty LabelProperty = 
+         DependencyProperty.Register( nameof( Label ), typeof( string ),
+                                      typeof( FilterPropertyControl ), new PropertyMetadata( string.Empty ) );
+      public string Label
+      {
+         get => (string)GetValue( LabelProperty );
+         set => SetValue( LabelProperty, value );
+      }
+
+      public FilterPropertyControl()
+      {
+         InitializeComponent();
+      }
+   }
+}
