75e329c102182598521e04aa6a4d1fe5e8111474
Unknown <zemoto@gmail.com>
2018-12-16 11:30:34 -0500

    Remove logic to rounds the interpolate target fps

--- Encoder/Filters/Video/Interpolate/InterpolateFilter.cs
+++ Encoder/Filters/Video/Interpolate/InterpolateFilter.cs
@@ -1,22 +1,9 @@
-﻿using System;
-
-namespace Encoder.Filters.Video.Interpolate
+﻿namespace Encoder.Filters.Video.Interpolate
 {
    internal sealed class InterpolateFilter : Filter
    {
       public override FilterViewModel ViewModel { get; } = new InterpolateFilterViewModel();
       public override string FilterName { get; } = "Interpolate";
-
-      public override void Initialize( double sourceFrameRate, TimeSpan sourceDuration )
-      {
-         base.Initialize( sourceFrameRate, sourceDuration );
-
-         var vm = (InterpolateFilterViewModel)ViewModel;
-
-         // Target the closest framerate that is multiple of the original framerate.
-         // This should help mitigate artifacting.
-         vm.TargetFrameRate = sourceFrameRate * Math.Floor( vm.TargetFrameRate / sourceFrameRate );
-      }
 
       public override bool ShouldApplyFilter() => ( (InterpolateFilterViewModel)ViewModel ).TargetFrameRate / SourceFrameRate > 1.5;
 
