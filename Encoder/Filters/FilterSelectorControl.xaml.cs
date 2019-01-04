3415c933f19e13338a902265ccf74008ef1bda12
David Markachev <zemoto@gmail.com>
2019-01-04 13:15:48 -0500

    Consolidate the two filter selector controls into one flexible control

---
+++ Encoder/Filters/FilterSelectorControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using ZemotoCommon.UI;
+
+namespace Encoder.Filters
+{
+   internal partial class FilterSelectorControl
+   {
+      public static readonly DependencyProperty LabelProperty = DependencyProperty.Register( 
+         nameof( Label ),
+         typeof( string ),
+         typeof( FilterSelectorControl ),
+         new PropertyMetadata( string.Empty ) );
+
+      public string Label
+      {
+         get => (string)GetValue( LabelProperty );
+         set => SetValue( LabelProperty, value );
+      }
+
+      public static readonly DependencyProperty OptionsContextProperty = DependencyProperty.Register( 
+         nameof( OptionsContext ),
+         typeof( ViewModelBase ),
+         typeof( FilterSelectorControl ),
+         new PropertyMetadata( null ) );
+
+      public ViewModelBase OptionsContext
+      {
+         get => (ViewModelBase)GetValue( OptionsContextProperty );
+         set => SetValue( OptionsContextProperty, value );
+      }
+
+      public FilterSelectorControl()
+      {
+         InitializeComponent();
+      }
+   }
+}
