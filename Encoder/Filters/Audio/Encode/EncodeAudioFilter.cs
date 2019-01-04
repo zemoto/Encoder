af1fe666e01b03de5de5ee251540fe88cb6855d1
David Markachev <zemoto@gmail.com>
2019-01-04 12:22:07 -0500

    +Audio Encode filter to change audio codec

---
+++ Encoder/Filters/Audio/Encode/EncodeAudioFilter.cs
@@ -0,0 +1,19 @@
+﻿using ZemotoCommon.Utils;
+
+namespace Encoder.Filters.Audio.Encode
+{
+   internal sealed class EncodeAudioFilter : AudioFilter
+   {
+      public override FilterViewModel ViewModel { get; } = new EncodeAudioFilterViewModel();
+      public override string FilterName { get; } = "Encode";
+
+      public override string CustomFilterArguments
+      {
+         get
+         {
+            var codec = ( (EncodeAudioFilterViewModel)ViewModel ).Codec;
+            return $"-c:a {codec.GetAttribute<FilterEnumValueAttribute>().ParameterValue}";
+         }
+      }
+   }
+}
---
+++ Encoder/Filters/Audio/Encode/EncodeAudioFilterViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Encoder.Filters.Audio.Encode
+{
+   internal enum AudioCodec
+   {
+      [FilterEnumValue( "AAC", "aac" )]
+      Aac,
+      [FilterEnumValue( "AC3", "ac3" )]
+      Ac3,
+      [FilterEnumValue( "FLAC", "flac" )]
+      Flac,
+      [FilterEnumValue( "OPUS", "opus" )]
+      Opus
+   }
+
+   [Filter]
+   internal sealed class EncodeAudioFilterViewModel : FilterViewModel
+   {
+      private AudioCodec _codec = AudioCodec.Aac;
+      [FilterParameter( "Audio Codec", "" )]
+      public AudioCodec Codec
+      {
+         get => _codec;
+         set => SetProperty( ref _codec, value );
+      }
+   }
+}
