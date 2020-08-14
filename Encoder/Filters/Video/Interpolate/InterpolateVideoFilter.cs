7002f3610e51c7ef90554021a46777f5e039f0e5
David <none@none.com>
2020-08-14 14:28:02 -0400

    Increase bitrate by 0.5x the increase in framerate instead of 1x
    That increased bitrate is overkill

--- Encoder/Filters/Video/Interpolate/InterpolateVideoFilter.cs
+++ Encoder/Filters/Video/Interpolate/InterpolateVideoFilter.cs
@@ -6,18 +6,19 @@ namespace Encoder.Filters.Video.Interpolate
    {
       public override FilterViewModel ViewModel { get; } = new InterpolateVideoFilterViewModel();
       public override string FilterName { get; } = "Interpolate";
+      private InterpolateVideoFilterViewModel InterpolateModel => (InterpolateVideoFilterViewModel)ViewModel;
 
-      public override bool CanApplyFilter() => Math.Abs( ( (InterpolateVideoFilterViewModel)ViewModel ).TargetFrameRate - SourceFrameRate ) > 0.001;
+      public override bool CanApplyFilter() => Math.Abs( InterpolateModel.TargetFrameRate - SourceFrameRate ) > 0.001;
 
       public override void Initialize( string file, double sourceFrameRate, TimeSpan sourceDuration )
       {
          base.Initialize( file, sourceFrameRate, sourceDuration );
-         TargetTotalFrames = (int)Math.Ceiling( SourceDuration.TotalSeconds * ( (InterpolateVideoFilterViewModel)ViewModel ).TargetFrameRate );
+         TargetTotalFrames = (int)Math.Ceiling( SourceDuration.TotalSeconds * InterpolateModel.TargetFrameRate );
       }
 
       public override uint GetFilterTargetBitRate( uint targetBitrateBeforeFilter )
       {
-         return (uint)( targetBitrateBeforeFilter * ( ( (InterpolateVideoFilterViewModel)ViewModel ).TargetFrameRate / SourceFrameRate ) );
+         return (uint)( 0.75 * targetBitrateBeforeFilter * InterpolateModel.TargetFrameRate / SourceFrameRate );
       }
    }
 }
